164fecadecb9f503d9a98afe9e832c1badda1266
Oliver Irvin <10015840@student.toiohomai.ac.nz>
2019-07-25 13:46:52 +1200

    all tasks completed

---
+++ task1/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name? ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + firstName + " " + lastName + ", Nice to meet you!");
+            Console.WriteLine("Hello {0} {1}, Nice to meet you!", firstName, lastName);
+            Console.WriteLine($"Hello {firstName} {lastName}, Nice to meet you!");
+        }
+    }
+}
---
+++ task1/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a whole number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please insert another whole number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please insert a decimal number:");
+            decimal dec1 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Please insert another decimal:");
+            decimal dec2 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please insert a string:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Please insert another string:");
+            string str2 = Console.ReadLine();
+
+            Console.WriteLine($"...Information...\n" +
+                $"The two numbers are {num1} and {num2}\n" +
+                $"The two decimals are {dec1} and {dec2}\n" +
+                $"The two strings are {str1} and {str2}");
+        }
+    }
+}
