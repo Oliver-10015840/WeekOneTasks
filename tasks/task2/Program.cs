6fd7552f6e97564a2c91da7f70dc6c2b7d0fcee2
Oliver Irvin <10015840@student.toiohomai.ac.nz>
2019-07-25 13:48:32 +1200

    commit 2 test

---
+++ tasks/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Age:");
+            double age = double.Parse(Console.ReadLine());
+            Console.WriteLine("Email:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Pin:");
+            int pin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n...Details...\nName {name}\nAddress: {address}\nAge: {age}\nEmail: {email} \nPin: {pin}");
+        }
+    }
+}
